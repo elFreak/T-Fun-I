6caf98481af070bad2cf5b57279ca8a44011aad6
elFreak <10chiefmarc10@gmail.com>
2017-05-10 15:37:09 +0200

    changes in InputCardBearbeiten

--- TfunI_GUI2/src/IOCard/InputCardBearbeiten.java
+++ TfunI_GUI2/src/IOCard/InputCardBearbeiten.java
@@ -14,6 +14,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import projectT_Fun_I.GlobalSettings;
 import userInterface.Controller;
@@ -27,27 +29,28 @@ import userInterface.MyBorderFactory;
 
 public class InputCardBearbeiten extends JPanel {
 	private static final long serialVersionUID = 1L;
-
+	// Attributte
+	// ------------------------------------------------------------------------------------------------------
 	Controller controller;
 
 	private JPanel panelFiltern = new JPanel(new GridBagLayout());
-	private JTextField tfFilter = new JTextField("10");
+	private JTextField tfFilter = new JTextField("0");
 	private JSlider sFilter = new JSlider();
-
 
 	private JPanel panelRahmen = new JPanel(new GridBagLayout());
 	private JTextField tfOffset = new JTextField("0");
 	private JTextField tfBereichUnten = new JTextField("0");
 	private JTextField tfBereichOben = new JTextField("0");
 	private JButton btAutoRahmen = new JButton("Auto");
-	
+
 	private JPanel panelSprung = new JPanel(new GridBagLayout());
 	private JTextField tfSprungzeit = new JTextField("10");
 	private JTextField tfSprunghöhe = new JTextField("10");
 	private JButton btSprung = new JButton("Sprung zurücksetzen");
+	// ------------------------------------------------------------------------------------------------------
 
-
-
+	// Konstruktor
+	// ------------------------------------------------------------------------------------------------------
 	/**
 	 * Konstructor
 	 * 
@@ -56,8 +59,10 @@ public class InputCardBearbeiten extends JPanel {
 	public InputCardBearbeiten(Controller controller) {
 		this.controller = controller;
 
-		// Textfelder werden konfiguriert:
-		// Anonymer MouseWheelListener für Testfeld
+		// Filtern
+		// -----------------------------------------------------
+
+		// Anonymer MouseWheelListener für Textfeld Filtern
 		tfFilter.addMouseWheelListener(new MouseWheelListener() {
 			@Override
 			public void mouseWheelMoved(MouseWheelEvent e) {
@@ -67,11 +72,73 @@ public class InputCardBearbeiten extends JPanel {
 					tempValue = 0;
 				}
 				tfFilter.setText(String.valueOf(tempValue));
+				if (tempValue > sFilter.getMaximum()) {
+					ChangeListener[] changeListener = sFilter.getChangeListeners();
+					sFilter.removeChangeListener(changeListener[0]);
+					sFilter.setValue(tempValue);
+					sFilter.addChangeListener(changeListener[0]);
+				} else
+					sFilter.setValue(tempValue);
+				controller.filterChanged(tempValue);
+			}
+		});
+		// Anonymer ActionListener für Textfeld Filter bei Enter
+		tfFilter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int tempValue = (int) Double.parseDouble(tfFilter.getText());
+				if (tempValue < 0) {
+					tempValue = 0;
+				}
+				tfFilter.setText(String.valueOf(tempValue));
+				if (tempValue > sFilter.getMaximum()) {
+					ChangeListener[] changeListener = sFilter.getChangeListeners();
+					sFilter.removeChangeListener(changeListener[0]);
+					sFilter.setValue(tempValue);
+					sFilter.addChangeListener(changeListener[0]);
+				} else
+					sFilter.setValue(tempValue);
 				controller.filterChanged(tempValue);
 			}
 		});
 
-		// Anonymer MouseWheelListener für Testfeld
+		// Schieberegler von Filtern konfigurieren
+		sFilter.setMinimum(0);
+		sFilter.setMaximum(30);
+		sFilter.setValue(0);
+		sFilter.setMinorTickSpacing(10);
+		sFilter.setMajorTickSpacing(10);
+		sFilter.setPaintTicks(true); // Striche werden angezeigt
+		sFilter.setPaintLabels(true); // Zahlen werden angezeigt
+		sFilter.setPaintTrack(true); // Balken wird angezeigt
+		sFilter.setOpaque(false);
+
+		// Anonymer ActionListener für Schieberegler
+		sFilter.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				int tempValue = sFilter.getValue();
+
+				tfFilter.setText(String.valueOf(tempValue));
+				controller.filterChanged(tempValue);
+			}
+		});
+
+		// Panel Filtern Konfigurieren
+		panelFiltern.setBackground(GlobalSettings.colorBackground);
+		panelFiltern.setBorder(MyBorderFactory.createMyBorder("Mittelwertfilter"));
+
+		panelFiltern.add(new JLabel("Intensit.:"), new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
+		panelFiltern.add(tfFilter, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.FIRST_LINE_START,
+				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
+		panelFiltern.add(sFilter, new GridBagConstraints(0, 1, 2, 1, 1.0, 0.0, GridBagConstraints.FIRST_LINE_START,
+				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
+		// -----------------------------------------------------
+
+		// Rahmen
+		// -----------------------------------------------------
+		// Anonymer MouseWheelListener für Textfeld Offset
 		tfOffset.addMouseWheelListener(new MouseWheelListener() {
 			@Override
 			public void mouseWheelMoved(MouseWheelEvent e) {
@@ -83,49 +150,6 @@ public class InputCardBearbeiten extends JPanel {
 				tfOffset.setText(String.valueOf(tempValue));
 				controller.setRange(Double.parseDouble(tfBereichOben.getText()),
 						Double.parseDouble(tfBereichUnten.getText()), Double.parseDouble(tfOffset.getText()));
-			}
-		});
-
-		// Anonymer MouseWheelListener für Testfeld
-		tfBereichUnten.addMouseWheelListener(new MouseWheelListener() {
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				double tempValue = Double.parseDouble(tfBereichUnten.getText());
-				tempValue += (e.getWheelRotation() * (0.05 * tempValue + 0.05));
-				if (tempValue < 0) {
-					tempValue = 0;
-				}
-				tfBereichUnten.setText(String.valueOf(tempValue));
-				controller.setRange(Double.parseDouble(tfBereichOben.getText()),
-						Double.parseDouble(tfBereichUnten.getText()), Double.parseDouble(tfOffset.getText()));
-			}
-		});
-
-		// Anonymer MouseWheelListener für Testfeld
-		tfBereichOben.addMouseWheelListener(new MouseWheelListener() {
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				double tempValue = Double.parseDouble(tfBereichOben.getText());
-				tempValue += (e.getWheelRotation() * (0.05 * tempValue + 0.05));
-				if (tempValue < 0) {
-					tempValue = 0;
-				}
-				tfBereichOben.setText(String.valueOf(tempValue));
-				controller.setRange(Double.parseDouble(tfBereichOben.getText()),
-						Double.parseDouble(tfBereichUnten.getText()), Double.parseDouble(tfOffset.getText()));
-			}
-		});
-
-		// Anonymer ActionListener für Textfeld bei Enter
-		tfFilter.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				int tempValue = (int) Double.parseDouble(tfFilter.getText());
-				if (tempValue < 0) {
-					tempValue = 0;
-				}
-				tfFilter.setText(String.valueOf(tempValue));
-				controller.filterChanged(tempValue);
 			}
 		});
 
@@ -144,6 +168,21 @@ public class InputCardBearbeiten extends JPanel {
 			}
 		});
 
+		// Anonymer MouseWheelListener für Textfeld BereichUnten
+		tfBereichUnten.addMouseWheelListener(new MouseWheelListener() {
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				double tempValue = Double.parseDouble(tfBereichUnten.getText());
+				tempValue += (e.getWheelRotation() * (0.05 * tempValue + 0.05));
+				if (tempValue < 0) {
+					tempValue = 0;
+				}
+				tfBereichUnten.setText(String.valueOf(tempValue));
+				controller.setRange(Double.parseDouble(tfBereichOben.getText()),
+						Double.parseDouble(tfBereichUnten.getText()), Double.parseDouble(tfOffset.getText()));
+			}
+		});
+
 		// Anonymer ActionListener für Textfeld bei Enter
 		tfBereichUnten.addActionListener(new ActionListener() {
 			@Override
@@ -156,6 +195,21 @@ public class InputCardBearbeiten extends JPanel {
 
 				controller.setRange(Double.parseDouble(tfBereichUnten.getText()),
 						Double.parseDouble(tfBereichOben.getText()), Double.parseDouble(tfOffset.getText()));
+			}
+		});
+
+		// Anonymer MouseWheelListener für Textfeld BereichOben
+		tfBereichOben.addMouseWheelListener(new MouseWheelListener() {
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				double tempValue = Double.parseDouble(tfBereichOben.getText());
+				tempValue += (e.getWheelRotation() * (0.05 * tempValue + 0.05));
+				if (tempValue < 0) {
+					tempValue = 0;
+				}
+				tfBereichOben.setText(String.valueOf(tempValue));
+				controller.setRange(Double.parseDouble(tfBereichOben.getText()),
+						Double.parseDouble(tfBereichUnten.getText()), Double.parseDouble(tfOffset.getText()));
 			}
 		});
 
@@ -174,27 +228,6 @@ public class InputCardBearbeiten extends JPanel {
 			}
 		});
 
-		// Panel Filtern konfigurieren
-		panelFiltern.setBackground(GlobalSettings.colorBackground);
-		panelFiltern.setBorder(MyBorderFactory.createMyBorder("Mittelwertfilter"));
-		sFilter.setMinimum(0);
-		sFilter.setMaximum(30);
-		sFilter.setValue(0);
-		sFilter.setMinorTickSpacing(10);
-		sFilter.setMajorTickSpacing(10);
-		sFilter.setPaintTicks(true);    //Striche werden angezeigt
-		sFilter.setPaintLabels(true);   //Zahlen werden angezeigt
-		sFilter.setPaintTrack(true);    //Balken wird angezeigt
-		sFilter.setOpaque(false);
-
-		panelFiltern.add(new JLabel("Intensit.:"), new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
-				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
-		panelFiltern.add(tfFilter, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.FIRST_LINE_START,
-				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
-		panelFiltern.add(sFilter, new GridBagConstraints(0, 1, 2, 1, 1.0, 0.0, GridBagConstraints.FIRST_LINE_START,
-				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
-		
-
 		// Panel Rahmen konfigurieren
 		panelRahmen.setBackground(GlobalSettings.colorBackground);
 		panelRahmen.setBorder(MyBorderFactory.createMyBorder("Offset und Bereich"));
@@ -212,7 +245,7 @@ public class InputCardBearbeiten extends JPanel {
 				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 20, 0));
 		panelRahmen.add(new JLabel("s"), new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
 				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
-		
+
 		panelRahmen.add(new Label("Ende:"), new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
 				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
 		panelRahmen.add(tfBereichUnten, new GridBagConstraints(1, 2, 1, 1, 1.0, 0.0,
@@ -222,11 +255,12 @@ public class InputCardBearbeiten extends JPanel {
 
 		panelRahmen.add(btAutoRahmen, new GridBagConstraints(0, 3, 3, 1, 0.0, 0.0, GridBagConstraints.CENTER,
 				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
-		
+
+		// Sprung
+		// --------------------------------------------------------
 		// Panel Sprung konfigurieren
 		panelSprung.setBackground(GlobalSettings.colorBackground);
 		panelSprung.setBorder(MyBorderFactory.createMyBorder("Sprung"));
-		
 
 		panelSprung.add(new JLabel("Sprungzeit:"), new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
 				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
@@ -240,12 +274,11 @@ public class InputCardBearbeiten extends JPanel {
 				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
 		panelSprung.add(new JLabel("V"), new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
 				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 0, 0));
-		panelSprung.add(btSprung, new GridBagConstraints(0, 2, 3, 1, 1.0, 0.0,
-				GridBagConstraints.FIRST_LINE_START, GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 20, 0));
-		
-		
-		
+		panelSprung.add(btSprung, new GridBagConstraints(0, 2, 3, 1, 1.0, 0.0, GridBagConstraints.FIRST_LINE_START,
+				GridBagConstraints.HORIZONTAL, new Insets(20, 10, 10, 10), 20, 0));
 
+		// Cardpanel
+		// -----------------------------------------------------------
 		// Cardpanel konfigurieren
 		this.setLayout(new GridBagLayout());
 		this.setBackground(GlobalSettings.colorBackground);
