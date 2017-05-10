4d91f57facb8062eaf46e4efb9b6858ceded839e
annahome <adrian.annaheim@students.fhnw.ch>
2017-05-10 09:52:20 +0200

    add target

--- MVCFrameWorkV2/src/pro2e/teamX/matlabfunctions/NelderMeadSimplexSinusCosExample.java
+++ MVCFrameWorkV2/src/pro2e/teamX/matlabfunctions/NelderMeadSimplexSinusCosExample.java
@@ -21,7 +21,7 @@ import org.apache.commons.math3.util.FastMath;
 public class NelderMeadSimplexSinusCosExample {
 
 	public static void main(String[] args) {
-		Filter filter = FilterFactory.createButter(6, 1.0);
+		Filter filter = FilterFactory.createButter(3, 1.0);
 		double[] t = Matlab.linspace(0, 15.0, 100);
 		
 		double[] step_soll = (double[])SVTools.step(filter.B, filter.A, t)[0];
@@ -36,23 +36,23 @@ public class NelderMeadSimplexSinusCosExample {
 
 		Complex[] rA = Matlab.roots(filter.A);
 		
-		//double[] startwerte = new double[] {1.0, rA[0].abs(), (rA[0].abs()/(-2.0*rA[0].getReal())), rA[0].getReal(),1.0,1.0,1.0  };
-		double[] startwerte = new double[] {1.0, 1.0, -1.0, 1.0 ,1.0,1.0 ,1.0};
+		double[] startwerte = new double[] {1.0, rA[0].abs(), (rA[0].abs()/(-2.0*rA[0].getReal())), rA[0].getReal()  };
+		//double[] startwerte = new double[] {1.0, 1.0, -1.0, 1.0 ,1.0,1.0 ,1.0};
 		
-		double[] s = target.omega2polstep(3, new double[] {1.0, rA[0].abs(), (rA[0].abs()/(-2.0*rA[0].getReal())), rA[0].getReal()}, t);
+		//double[] s = target.omega2polstep(3, new double[] {1.0, rA[0].abs(), (rA[0].abs()/(-2.0*rA[0].getReal())), rA[0].getReal()}, t);
 		
 		//		//FourExtrema fourExtrema = new FourExtrema();
 //
 		PointValuePair optimum = optimizer.optimize(new MaxEval(1000), new ObjectiveFunction(target), GoalType.MINIMIZE,
-				new InitialGuess(startwerte), new NelderMeadSimplex(new double[] { 0.2, 0.2, 0.2, 0.2,0.2,0.2,0.2 }));
+				new InitialGuess(startwerte), new NelderMeadSimplex(new double[] { 0.2, 0.2, 0.2, 0.2 }));
 //
 		System.out.println(Arrays.toString(optimum.getPoint()) + " : " + optimum.getSecond());
 		System.out.println(""+rA[0].abs());
 		System.out.println(""+rA[0].abs()/(-2.0*rA[0].getReal()));
 		System.out.println(""+rA[0].getReal());
 		
-		double[] step_approx = target.omega2polstep(6, optimum.getPoint(), t);
-		for (int i = 0; i < step_soll.length; i++) {
+		double[] step_approx = target.omega2polstep(3, optimum.getPoint(), t);
+		for (int i = 0; i < step_approx.length; i++) {
 			System.out.println(""+step_approx[i]);
 		}
 	}
@@ -67,78 +67,6 @@ public class NelderMeadSimplexSinusCosExample {
 	//	}
 }
 
-class Target implements MultivariateFunction {
-
-	private double[] t;
-	private double[] step_soll;
-	private int ord;
-
-	//Approximation approximation;
-	//ApproximationSettings approximationsettings = new ApproximationSettings();
-
-	public Target(double[] t, double[] step_soll, int ord) {
-		this.t = t;
-		this.step_soll = step_soll;
-		this.ord = ord;
-	}
-
-	public double value(double[] variables) {
-		final double x0 = variables[0];
-		final double x1 = variables[1];
-		final double x2 = variables[2];
-		final double x3 = variables[3];
-		final double x4 = variables[4];
-		final double x5 = variables[5];
-		final double x6 = variables[6];
-		//final double x7 = variables[7];
-		
-
-
-		return failuresum(step_soll, omega2polstep(ord, variables, t));
-
-	}
-
-	//x0=[K wp1 qp1 wp2 qp2 wp3 qp3]        n=even
-	// x0=[K wp1 qp1 wp2 qp2 wp3 qp3 sig]    n=odd
-
-	public static double[] omega2polstep(int ordnung, double[] data, double[] time) {
-		double[] zaehler1 = new double[1];
-		double[] nenner1 = new double[ordnung + 1];
-		double[] nenner2 = new double[ordnung + 1];
-		double[] data2 = new double[time.length];
-		if (ordnung % 2 == 0) {
-			zaehler1[0] = data[0] * Math.pow(data[1], 2.0);
-			nenner1 = new double[] { 1, data[1] / data[2], Math.pow(data[1], 2.0) };
-			for (int i = 2; i < ordnung - 1; i += 2) {
-				nenner2 = new double[] { 1, data[i + 1] / data[i + 2], Math.pow(data[i + 1], 2.0) };
-				nenner1 = Matlab.conv(nenner1, nenner2);
-				zaehler1[0] = zaehler1[0] * Math.pow(data[i + 1], 2.0);
-			}
-		} else {
-			zaehler1[0] = data[0] * Math.pow(data[1], 2.0);
-			nenner1 = new double[] { 1, data[1] / data[2], Math.pow(data[1], 2.0) };
-			for (int i = 2; i < ordnung - 1; i += 2) {
-				nenner2 = new double[] { 1, data[i + 1] / data[i + 2], Math.pow(data[i + 1], 2.0) };
-				nenner1 = Matlab.conv(nenner1, nenner2);
-				zaehler1[0] = zaehler1[0] * Math.pow(data[i + 1], 2.0);
-			}
-			nenner1 = Matlab.conv(new double[] { 1, -data[ordnung] }, nenner1);
-			zaehler1[0] = zaehler1[0] * Math.abs(data[ordnung]);
-		}
-		data2 = (double[]) SVTools.step(zaehler1, nenner1, time)[0];
-		return data2;
-	}
-
-	private static double failuresum(double[] should, double[] is) {
-
-		double failuresum = 0;
-
-		for (int i = 0; i < is.length; i++) {
-			failuresum = failuresum + Math.pow(should[i] - is[i], 2);
-		}
-		return failuresum;
-	}
-}
 
 //	private static class FourExtrema implements MultivariateFunction
 //    {
