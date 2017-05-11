40f63d97901340deef1d117db9aa042ddc4a70f0
PiusOchs <pius.ochs@students.fhnw.ch>
2017-05-11 14:52:06 +0200

    Data Read
    Daten lesen

--- DataRead/DataRead.java
+++ DataRead/DataRead.java
@@ -1,6 +1,7 @@
 package userInterface;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import javax.swing.JFileChooser;
@@ -8,64 +9,113 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class DataRead {
 	public static double[][] csvread() {
+		JFileChooser chooser = new JFileChooser();
+		BufferedReader eingabeDatei = null;
+		File f = null;
 		double[][] data = null;
+		double[][] returndata = null;
 		int nLines = 0;
 		int nColumns = 0;
-		final JFileChooser chooser = new JFileChooser("Open");
-		
+		boolean lengtherror = false;
+		boolean negativetime = false;
 
 		try {
-			//Filechooser Filter einstellen
-			FileNameExtensionFilter filter = new FileNameExtensionFilter("csv Datei","csv");
-			chooser.setFileFilter(filter);
-			//File auswählen und Pfad bestimmen
+			// Filechooser Filter einstellen
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("csv Datei", "csv");
+			chooser.addChoosableFileFilter(filter);
+			chooser.removeChoosableFileFilter(chooser.getChoosableFileFilters()[0]);
+
+			// File auswählen und Pfad bestimmen
 			chooser.showOpenDialog(chooser);
-			String Filepath = chooser.getSelectedFile().getPath();
-			// Anzahl Zeilen und Anzahl Kolonnen festlegen und festlegen ob Spalten oder Zeilenvektor vorhanden ist:
-			BufferedReader eingabeDatei = new BufferedReader(new FileReader(Filepath));
-			String[] s = eingabeDatei.readLine().split("[, ]+");
-			int t = s.length;
-			if (t<=3) {
-				nColumns = s.length;
-				nLines++;
-				while (eingabeDatei.readLine() != null) {
-					nLines++;
-				}
-				// Gezählte Anzahl Zeilen und Kolonnen lesen:
+			f = chooser.getSelectedFile();
+			if (f == null) {
+
+			} else {
+				String Filepath = f.getPath();
+				// Anzahl Zeilen und Anzahl Kolonnen festlegen und festlegen ob
+				// Spalten oder Zeilenvektor vorhanden ist:
 				eingabeDatei = new BufferedReader(new FileReader(Filepath));
-				data = new double[nColumns][nLines];
-				for (int i = 0; i < data[0].length; i++) {
-					s = eingabeDatei.readLine().split("[, ]+");
-					for (int k = 0; k < s.length; k++) {
-						data[k][i] = Double.parseDouble(s[k]);
+
+				String[] s = eingabeDatei.readLine().split("[, ]+");
+				int t = s.length;
+				eingabeDatei.close();
+				eingabeDatei = new BufferedReader(new FileReader(Filepath));
+				
+				//Je nachdem, ob es sich bei der csv Datei um Spalten oder Zeilenvektoren handelt, 
+				//wird die Datei anders eingelesen.
+				if (t <= 3) {
+					nColumns = s.length;
+					while (eingabeDatei.readLine() != null) {
+						nLines++;
+					}
+
+					// Gezählte Anzahl Zeilen und Kolonnen lesen:
+					eingabeDatei.close();
+					eingabeDatei = new BufferedReader(new FileReader(Filepath));
+					data = new double[nColumns][nLines];
+					for (int i = 0; i < nLines; i++) {
+						s = eingabeDatei.readLine().split("[, ]+");
+						//Abfrage, ob alle Vektoren dieselbe Länge besitzten.
+						if (s.length != nColumns) {
+							lengtherror = true;
+						}
+						for (int k = 0; k < s.length; k++) {
+							data[k][i] = Double.parseDouble(s[k]);
+						}
+					}
+					//Je nachdem, ob es sich bei der csv Datei um Spalten oder Zeilenvektoren handelt, 
+					//wird die Datei anders eingelesen.
+				} else {
+					nLines = s.length;
+					while (eingabeDatei.readLine() != null) {
+						nColumns++;
+					}
+
+					// Gezählte Anzahl Zeilen und Kolonnen lesen:
+					eingabeDatei.close();
+					eingabeDatei = new BufferedReader(new FileReader(Filepath));
+					data = new double[nColumns][nLines];
+					for (int i = 0; i < nColumns; i++) {
+						s = eingabeDatei.readLine().split("[, ]+");
+						//Abfrage, ob alle Vektoren dieselbe Länge besitzten.
+						if (s.length != nLines) {
+							lengtherror = true;
+						}
+						for (int k = 0; k < s.length; k++) {
+							data[i][k] = Double.parseDouble(s[k]);
+						}
 					}
 				}
 			}
-			else {
-				nLines = s.length;
-				nColumns++;
-				while (eingabeDatei.readLine() != null) {
-					nColumns++;
+			for (int i = 0; i < data[0].length; i++) {
+				if (data[0][i] < 0) {
+					negativetime = true;
 				}
-				
-
-				// Gezählte Anzahl Zeilen und Kolonnen lesen:
-				eingabeDatei = new BufferedReader(new FileReader(Filepath));
-				data = new double[nColumns][nLines];
-				for (int i = 0; i < nColumns; i++) {
-					s = eingabeDatei.readLine().split("[, ]+");
-					for (int k = 0; k < data[0].length; k++) {
-						data[i][k] = Double.parseDouble(s[k]);
-					}
-				}
-				
 			}
-			eingabeDatei.close();
-
 			
+		//Fehler werden abgefangen.
 		} catch (IOException exc) {
-			System.err.println("Dateifehler: " + exc.toString());
+
+		} 
+		catch (NumberFormatException exc) {
+			if (f.getName().endsWith(".csv")) {
+				String s = exc.toString();
+				System.out.println(s);
+				StatusBar.showStatus("Datei enthällt nicht nur Zahlen. Fehler bei Dateipunkt " + s.substring(50) + ".");
+			}
+		} catch (NullPointerException exc) {
+			StatusBar.showStatus("Datei enthällt keine Daten.");
 		}
-		return data;
+
+		if (lengtherror) {
+			StatusBar.showStatus("Die Vektoren besitzen nicht dieselbe Länge.");
+		} else if (negativetime) {
+			StatusBar.showStatus("Der Zeitvektor darf keine negativen Werte beinnhalten.");
+		} else {
+			returndata = data;
+		}
+		//Daten werden zurückgegeben.
+		return returndata;
 	}
+
 }
